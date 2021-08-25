602dbc14c076dca862f54375bbf8b68c2d18411d
lahiardhan <lahia.ardhanm@gmail.com>
2021-08-25 16:58:01 +0700

    Tugas 14 JS
    Tugas ini saya serahkan kepada Tim Niomic

---
+++ tugas14.js
@@ -0,0 +1,9 @@
+let x = 37;
+let tanggal = 15;
+
+let pembagian = x/tanggal;
+
+console.log(pembagian);
+console.log(Math.round(pembagian));
+console.log(Math.ceil(pembagian));
+console.log(Math.floor(pembagian));
